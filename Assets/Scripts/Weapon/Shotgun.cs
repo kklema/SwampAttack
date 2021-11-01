142be61e00d1520b75df04646a8fc011f3db771e
kklema <ownedrus@gmail.com>
2021-11-01 17:18:36 +0300

    correct shotgun script
    for delete

--- Assets/Scripts/Weapon/Shotgun.cs
+++ Assets/Scripts/Weapon/Shotgun.cs
@@ -6,13 +6,10 @@ public class Shotgun : Weapon
 {
     public override void Shoot(Transform shootPoint)
     {
-        for (int i = 1; i <= 3; i++)
-        {
-            Bullet tempBullet = Instantiate(Bullet, shootPoint.position, Quaternion.identity);
-            tempBullet = Instantiate(Bullet, shootPoint.position, Quaternion.identity);
-            tempBullet.SetDirection(new Vector2(-1f, 0.2f));
-            tempBullet = Instantiate(Bullet, shootPoint.position, Quaternion.identity);
-            tempBullet.SetDirection(new Vector2(-1f, -0.2f));
-        }
+        Bullet tempBullet = Instantiate(Bullet, shootPoint.position, Quaternion.identity);
+        tempBullet = Instantiate(Bullet, shootPoint.position, Quaternion.identity);
+        tempBullet.SetDirection(new Vector2(-1f, 0.2f));
+        tempBullet = Instantiate(Bullet, shootPoint.position, Quaternion.identity);
+        tempBullet.SetDirection(new Vector2(-1f, -0.2f));
     }
 }
